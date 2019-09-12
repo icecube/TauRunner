ab554f440fcc3d1d8104c2fe73566371c7f8b85c
Ibrahim Safa <isafa@cobalt08.icecube.wisc.edu>
2019-09-12 12:05:30 -0500

    added decay parametrization

--- ForbiddenMC/Casino.py
+++ ForbiddenMC/Casino.py
@@ -9,24 +9,10 @@ import subprocess
 import nuSQUIDSpy as nsq
 
 units = nsq.Const()
-gr = nsq.GlashowResonanceCrossSection()
-dis = nsq.NeutrinoDISCrossSectionsFromTables()
-tds = nsq.TauDecaySpectra()
 
-tau_propagate_path = sys.path[-1] + 'propagate_taus.sh'
-
-#cross sections patched with nuSQUIDS 
-#temporary until EHE cross sections are added to nuSQUIDS
+#cross section tables
 ######################################
 cross_section_path = sys.path[-1]+'../cross_sections/'
-#charged = np.load(cross_section_path + 'nuXS_CC_8-16.npy')
-#neutral = np.load(cross_section_path + 'nuXS_NC_8-16.npy')
-#energies = np.logspace(17, 25, 500)
-#energies = (energies[:-1] + energies[1:])/2
-
-#log_e = np.log10(energies)
-#log_XS_CC = np.log10(charged)
-#log_XS_NC = np.log10(neutral)
 
 f_NC = np.load(cross_section_path+'NC_table.npy').item()
 f_CC = np.load(cross_section_path+'CC_table.npy').item()
@@ -37,6 +23,7 @@ dsdy_spline_NC = np.load(cross_section_path + 'dsigma_dy_NC.npy').item()
 dsdy_spline_NC_lowe = np.load(cross_section_path + 'dsigma_dy_NC_lowE.npy').item()
 
 #####################################
+#Cross section functions
 
 def TotalNeutrinoCrossSection(enu,
 		      flavor = nsq.NeutrinoCrossSections_NeutrinoFlavor.tau,
@@ -59,14 +46,13 @@ def TotalNeutrinoCrossSection(enu,
 	    Total neutrino cross section at the given energy in natural units.
 	'''
 
-#	if(np.log10(enu) > log_e[0]):
-
+	if(np.log10(enu) < 0.):
+	    print("Going below a GeV. this region is not supported. godspeed!")
+	    return 0.
 	if(interaction == nsq.NeutrinoCrossSections_Current.NC):
 	    return((10**f_NC(np.log10(enu/1e9)))*(units.cm)**2)
         else:
             return((10**f_CC(np.log10(enu/1e9)))*(units.cm)**2)
-#	else:
-#	    return dis.TotalCrossSection(enu,flavor,neutype,interaction)*(units.cm)**2
 
 def DifferentialOutGoingLeptonDistribution(ein, eout, interaction):
 	r'''
@@ -172,6 +158,7 @@ def GetDistancesPerSection(theta, radii):
 ###########################################
 ##### Earth Model handling ends here ######
 ###########################################
+tau_propagate_path = sys.path[-1] + 'propagate_taus.sh'
 
 def DoAllCCThings(objects):
     r'''
@@ -222,17 +209,79 @@ def DoAllCCThings(objects):
 ############## Tau Decay Parameterization ################
 ##########################################################
 
+RPion = 0.07856**2 
+RRho = 0.43335**2 
+RA1 = 0.70913**2
+BrLepton = 0.18
+BrPion = 0.12
+BrRho = 0.26
+BrA1 = 0.13
+BrHad = 0.13
+
+def TauDecayToLepton(Etau, Enu, P):
+    z = Enu/Etau
+    g0 = (5./3.) - 3.*z**2 + (4./3.)*z**3
+    g1 = (1./3.) - 3.*z**2 + (8./3.)*z**3
+    return(g0+P*g1)
+
+def TauDecayToPion(Etau, Enu, P):
+    z = Enu/Etau
+    g0 = 0.
+    g1 = 0.
+    if((1. - RPion - z)  > 0.0):
+        g0 = 1./(1. - RPion)
+        g1 = -(2.*z - 1. - RPion)/(1. - RPion)**2
+
+    return(g0+P*g1)
+    
+def TauDecayToRho(Etau, Enu, P):
+    z = Enu/Etau
+    g0 = 0.
+    g1 = 0.
+    if((1. - RRho - z) > 0.0):
+        g0 = 1./(1. - RRho)
+        g1 = -((2.*z-1.+RRho)/(1.-RRho))*((1.-2.*RRho)/(1.+2.*RRho))
+    return(g0+P*g1)
+
+def TauDecayToA1(Etau, Enu, P):
+    z = Enu/Etau
+    g0 = 0.
+    g1 = 0.
+    if((1. - RA1 - z) > 0.0):
+        g0 = (1./(1.-RA1))
+        g1 = -((2.*z-1.+RA1)/(1.-RA1))*((1.-2.*RA1)/(1.+2.*RA1))
+    return(g0 + P*g1)
+
+def TauDecayToHadrons(Etau, Enu, P):
+    z = Enu/Etau
+    g0=0.
+    g1=0.
+    if((0.3 - z) > 0.):
+        g0 = 1./0.3
+    return(g0+P*g1)
+
+def TauDecayToAll(Etau, Enu, P):
+    decay_spectra = 0
+    decay_spectra+=2.0*BrLepton*TauDecayToLepton(Etau, Enu, P)
+    decay_spectra+=BrPion*TauDecayToPion(Etau, Enu, P)
+    decay_spectra+=BrRho*TauDecayToRho(Etau, Enu, P)
+    decay_spectra+=BrA1*TauDecayToA1(Etau, Enu, P)
+    decay_spectra+=BrHad*TauDecayToHadrons(Etau, Enu, P)
+    
+    return decay_spectra
+
 Etau = 100.
 zz = np.linspace(0.0,1.0,500)[1:-1]
-dNTaudz = lambda z: tds.TauDecayToAll(Etau, Etau*z)
+dNTaudz = lambda z: TauDecayToAll(Etau, Etau*z, 0.)
 TauDecayWeights = np.array(map(dNTaudz,zz))
 TauDecayWeights = TauDecayWeights/np.sum(TauDecayWeights)
-
 yy = np.linspace(0.0,1.0,300)[1:-1]
 
 ##########################################################
+#########################################################
 proton_mass = ((0.9382720813+0.9395654133)/2.)*units.GeV
 
+# Event object. where all relevant information about propagation is stored
 class CasinoEvent(object):
     r'''
     This is the class that contains all relevant event information stored in an object.
@@ -504,6 +553,8 @@ class CasinoEvent(object):
         print "id", self.particle_id, \
               "energy ", self.energy/units.GeV, " GeV", \
               "position ", self.position/units.km, " km"
+
+#This is the propagation algorithm. The MC meat, if you may.
 
 def RollDice(event):
 
