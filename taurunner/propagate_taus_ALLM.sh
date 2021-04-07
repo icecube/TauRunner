3194562af2a334a8a1edf582f0fb266d322500ec
Alex Pizzuto <pizzuto@wisc.edu>
2021-04-07 13:29:52 -0500

    env

--- taurunner/propagate_taus_ALLM.sh
+++ taurunner/propagate_taus_ALLM.sh
@@ -5,7 +5,7 @@ last_idx=$(( ${#Array[@]} - 1 ))
 rho=${Array[$last_idx]}
 unset Array[$last_idx]
 
-DIR=$PWD/
+DIR=$TAURUNNER_PATH
 
 awk -v var="${Array[*]}" "BEGIN {n=split(var , array); for (i=1; i<=n/2; i++) print array[2*i-1], array[2*i]}" | ${DIR}/../MMC/MMC/ammc -run -frejus -tau -medi="Frejus rock" -radius=1e6 -vcut=1.e-3 -rho=$rho -scat -lpm -bs=1 -ph=4 -bb=1 -sh=1 -ebig=1e21 -tdir=${DIR}../MMC/tables/
 
