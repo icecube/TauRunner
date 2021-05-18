7a702b0b1c13b7a1cd372b2426052bd06582f82e
Alex Pizzuto <pizzuto@wisc.edu>
2021-05-18 15:16:22 -0500

    load the file that jeff made

--- test/test_attenuation.py
+++ test/test_attenuation.py
@@ -1,29 +1,38 @@
-import unittest
+import unittest, os
 from taurunner import Casino
-from taurunner.main import propagate_neutrinos
+from taurunner.main import run_MC
 from taurunner.body import Body
 
 import numpy as np
 
-# include number conservation
-# include attenuation check
-# include energy conservation check 
+def find_nearest_ind(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
+
 class TestConservation(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """ once before all tests """
         num_sim = 1000
         ens = [1e5, 1e6, 1e7]
+        test_path = os.path.dirname(os.path.realpath(__file__))
+        cls.tabled_comparisons = np.load(f'{test_path}/.comparisons_for_tests/attenuation.npy')
+        cls.tabled_ens = cls.tabled_comparisons[0]
+        cls.tabled_attens = cls.tabled_comparisons[1]
+        table_inds = [find_nearest_ind(cls.tabled_ens, en*1e9) for en in ens]
+        cls.atten_dict = {cls.tabled_ens[ind]/1e9: cls.tabled_attens[ind] for ind in table_inds}
         cls.sim_dict = dict()
         body = Body(6.0, 6368.0) # Construct earthsized body of constant density
-        for en in ens:
-            sim = propagate_neutrinos(num_sim, 1, theta=60.,
+        for en in cls.atten_dict.keys():
+            sim = run_MC(num_sim, 1, theta=60.,
                                       debug=False, xs_model='dipole', 
-                                      energy=en, body=body, losses=False,
+                                      energy=en, body=body, 
+                                      losses=False,
                                      )
             cls.sim_dict[en] = sim
         cls.num_sim = num_sim
-        cls.atten_dict = {1e5: 0.63, 1e6: 0.22, 1e7: 0.012}
+        cls.sim_dict_ens = list(cls.sim_dict.keys())
 
     @classmethod
     def tearDownClass(cls):
@@ -39,37 +48,33 @@ class TestConservation(unittest.TestCase):
         pass
 
     def test_energies(self):
-        incoming_en = 0.
-        outgoing_en = 0.
-        for ptype in self.sim_dict[1e6]:
-            incoming_en += np.sum(ptype['Eini'])
-            outgoing_en += np.sum(ptype['Eout'])
+        en_key = self.sim_dict_ens[0]
+        incoming_en = np.sum(self.sim_dict[en_key]['Eini'])
+        outgoing_en = np.sum(self.sim_dict[en_key]['Eout'])
         self.assertTrue(incoming_en >= outgoing_en)
 
     def test_number_conservation(self):
-        outgoing_n = 0
-        for ptype in self.sim_dict[1e6]:
-            outgoing_n += len(ptype['Eini'])
+        en_key = self.sim_dict_ens[0]
+        outgoing_n = len(self.sim_dict[en_key]['Eini'])
         self.assertTrue(outgoing_n == self.num_sim)
 
     def test_attenuation(self):
         relative_diffs = {}
         for en in self.sim_dict.keys():
-            noninteracting = 0
-            for ptype in self.sim_dict[en]:
-                msk = ptype['nCC'] == 0
-                msk *= ptype['nNC'] == 0
-                noninteracting += len(ptype['Eini'][msk])
+            msk = self.sim_dict[en]['nCC'] == 0
+            msk *= self.sim_dict[en]['nNC'] == 0
+            noninteracting = len(self.sim_dict[en]['Eini'][msk])
             survival_prob = float(noninteracting) / float(self.num_sim)
             diff = (survival_prob - self.atten_dict[en]) \
                 / self.atten_dict[en]
             diff = np.abs(diff)
             relative_diffs[en] = diff
             print(en, survival_prob, diff)
-        self.assertTrue(relative_diffs[1e5] < 0.1)
-        self.assertTrue(relative_diffs[1e6] < 0.1)
-        # increase threshold for 1e7 because low statistics
-        self.assertTrue(relative_diffs[1e7] < 0.5)
+            print(self.atten_dict[en])
+            print()
+        self.assertTrue(relative_diffs[self.sim_dict_ens[0]] < 0.1)
+        self.assertTrue(relative_diffs[self.sim_dict_ens[1]] < 0.1)
+        self.assertTrue(relative_diffs[self.sim_dict_ens[2]] < 0.1)
 
 
 if __name__ == '__main__':
