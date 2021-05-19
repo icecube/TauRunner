8cde5620ca2a473ee2eec1b8580f783cea2caac4
Alex Pizzuto <pizzuto@wisc.edu>
2021-05-19 10:59:21 -0500

    tests passing for once

--- test/test_attenuation.py
+++ test/test_attenuation.py
@@ -14,7 +14,7 @@ class TestConservation(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """ once before all tests """
-        num_sim = 50
+        num_sim = 200
         ens = [1e5, 1e6, 1e7]
         test_path = os.path.dirname(os.path.realpath(__file__))
         cls.tabled_comparisons = np.load(f'{test_path}/.comparisons_for_tests/attenuation.npy')
@@ -54,24 +54,23 @@ class TestConservation(unittest.TestCase):
         self.assertTrue(incoming_en >= outgoing_en)
 
     def test_number_conservation(self):
-        en_key = self.sim_dict_ens[0]
-        outgoing_n = len(self.sim_dict[en_key]['Eini'])
-        self.assertTrue(outgoing_n == self.num_sim)
+        en_key = self.sim_dict_ens[-1]
+        nutau_msk = self.sim_dict[en_key]['PDG_Encoding'] == 16
+        outgoing_nutau = len(self.sim_dict[en_key]['Eini'][nutau_msk])
+        self.assertTrue(outgoing_nutau <= self.num_sim)
 
     def test_attenuation(self):
         relative_diffs = {}
         for en in self.sim_dict.keys():
             msk = self.sim_dict[en]['nCC'] == 0
             msk *= self.sim_dict[en]['nNC'] == 0
-            noninteracting = len(self.sim_dict[en]['Eini'][msk])
+            nutau_msk = self.sim_dict[en]['PDG_Encoding'] == 16
+            noninteracting = len(self.sim_dict[en]['Eini'][msk*nutau_msk])
             survival_prob = float(noninteracting) / float(self.num_sim)
             diff = (survival_prob - self.atten_dict[en]) \
                 / self.atten_dict[en]
             diff = np.abs(diff)
             relative_diffs[en] = diff
-            print(en, survival_prob, diff)
-            print(self.atten_dict[en])
-            print()
         self.assertTrue(relative_diffs[self.sim_dict_ens[0]] < 0.1)
         self.assertTrue(relative_diffs[self.sim_dict_ens[1]] < 0.1)
         self.assertTrue(relative_diffs[self.sim_dict_ens[2]] < 0.1)
