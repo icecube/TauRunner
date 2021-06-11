cross section units bug. secondaries reproducibility fixed

taurunner/Casino.py
@@ -310,7 +310,7 @@ class Particle(object):
             self.SetParticleProperties()
             if self.secondaries:
                 # sample branching ratio of tau leptonic decay
-                p0 = np.random.uniform(0,1)
+                p0 = self.rand.uniform(0,1)
                 bins = list(np.logspace(-5,0,101))[:-1]
                 if p0 < .18:
                     xs_path = os.path.dirname(os.path.realpath(__file__)) + '/cross_sections/secondaries_splines/'

taurunner/cross_sections/xs.py
@@ -11,7 +11,7 @@ def hima_tot_xs(E, spl):
     pass
 
 def jeff_tot_xs(E, spl):
-    return np.exp(spl(np.log(E)))
+    return np.exp(spl(np.log(E/1e9)))
 
 def hima_diff_xs(E_in, E_out, spl):
     return(10**spl(np.log10(E_in), np.log10(E_out))[0][0]/E_in)