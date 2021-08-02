5ddf2e2a7b90ade52861d26dac865aac333b408d
ibsafa <isafa@wisc.edu>
2021-08-02 15:27:15 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,9 @@ setuptools.setup(
     #long_description_content_type="text/markdown",
     url="https://github.com/icecube/TauRunner",
     packages=setuptools.find_packages(),
+    install_requires=['numpy>=1.16.6',
+                      'scipy>=1.2.3',
+                      'proposal==6.1.6'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
