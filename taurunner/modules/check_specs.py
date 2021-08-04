e3586bbc05df7a4b24ab2975588850ea21d80387
jlazar17 <jeff.p.lazar@gmail.com>
2021-08-04 15:52:07 -0500

    Remove check_specs.py

--- taurunner/modules/check_specs.py
+++
@@ -1,8 +0,0 @@
-def check_specs(TR_specs):
-    
-    if TR_specs['nevents'] <= 0:
-        raise RuntimeError('You must specify more than 0 events to simulate (-n)') 
-    # Clean up args so that there is angular arg and energy args
-    if (not gzk and theta is None) or (energy is None and spectrum is None):
-        raise RuntimeError('You must either pick an energy and theta, use a spectrum, or use the GZK flux')
-    
