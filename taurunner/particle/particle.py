2bf49faf4f3b9c48ee7c531a0444f5e2f05823f3
ibsafa <isafa@wisc.edu>
2021-08-18 11:52:43 -0500

    fixed bug for escaping (non-decaying) taus

--- taurunner/particle/particle.py
+++ taurunner/particle/particle.py
@@ -197,7 +197,7 @@ class Particle(object):
         total_dist       = track.x_to_d(1.-self.position)*body.radius/units.km
         current_density  = body.get_average_density(track.x_to_r(self.position))
         km_dist_to_prop  = total_dist - current_km_dist
-        
+
         if(np.logical_and(np.abs(self.ID) in [13, 14], km_dist_to_prop > 100.)):
              return
 
@@ -217,8 +217,12 @@ class Particle(object):
         lep_length     = final_vec.magnitude() / 1e5
         decay_products = [p for i,p in zip(range(max(len(particles)-3,0),len(particles)), particles[-3:]) if int(p.type) <= 1000000001]
         en_at_decay    = np.sum([p.energy for p in decay_products])
-        self.energy    = en_at_decay*units.GeV/1e3
-        self.chargedposition  = lep_length
+        if(en_at_decay==0):    #particle reached the border before decaying
+            self.energy = particles[-1].parent_particle_energy*units.GeV/1e3
+            self.chargedposition = float(np.ceil(lep_length))
+        else:                  #particle decayed before reaching the border
+            self.energy    = en_at_decay*units.GeV/1e3
+            self.chargedposition  = lep_length
         return
 
     def Interact(self, interaction, body=None, track=None): #  dist_to_prop=None, current_density=None):
