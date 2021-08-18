2320768ea59159d0f2982fc43034b0347836796a
jlazar17 <jeff.p.lazar@gmail.com>
2021-08-18 08:47:15 -0500

    Add xs_model module

--- taurunner/utils/make_initial_e.py
+++ taurunner/utils/make_initial_e.py
@@ -13,11 +13,11 @@ def make_initial_e(TR_specs, rand=None):
         else:
             eini = np.full(TR_specs['nevents'], e)*units.GeV
     else:
-        # TODO figure out this part lol
         if not os.path.isfile(TR_specs['energy']):
             raise RuntimeError(f"GZK CDF Spline file {TR_specs['energy']} does not exist")
         # sample initial energies and incoming angles from GZK parameterization
-        cdf_indices = rand.uniform(low=0., high=1.,size=TR_specs['nevents'])
+        cdf_indices = rand.uniform(low=0., high=1., size=TR_specs['nevents'])
+        # TODO figure out this part lol
         cdf         = np.load(TR_specs['energy'], allow_pickle=True, encoding='latin1').item()
         eini        = cdf(cdf_indices)*units.GeV
     return eini
