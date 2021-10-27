e68cc2a38de6408d0554935e3f701c817ef1261c
jlazar17 <jeff.p.lazar@gmail.com>
2021-10-26 23:36:08 -0500

    Change units to ev

--- taurunner/utils/make_initial_e.py
+++ taurunner/utils/make_initial_e.py
@@ -27,9 +27,9 @@ def make_initial_e(nevents, energy, e_min=None, e_max=None, rand=None):
     if is_floatable(energy):
         e = float(energy)
         if e<=0:
-            eini = sample_powerlaw(rand, e_min, e_max, e + 1, size=nevents)*units.GeV
+            eini = sample_powerlaw(rand, e_min, e_max, e + 1, size=nevents)
         else:
-            eini = np.full(nevents, e)*units.GeV
+            eini = np.full(nevents, e)
     else:
         if not os.path.isfile(energy):
             raise RuntimeError(f"Spline file {energy} does not exist")
@@ -41,5 +41,5 @@ def make_initial_e(nevents, energy, e_min=None, e_max=None, rand=None):
             with open(energy, 'rb') as pkl_f:
                 cdf = pkl.load(pkl_f)
         cdf_indices = rand.uniform(low=0., high=1.,size=nevents)
-        eini        = cdf(cdf_indices)*units.GeV
+        eini        = cdf(cdf_indices)
     return eini
