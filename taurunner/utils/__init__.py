cab5aa2182c1d33f782e16309c207387fe35a6c2
jlazar17 <jeff.p.lazar@gmail.com>
2024-04-15 16:28:36 -0500

    .

--- taurunner/utils/__init__.py
+++ taurunner/utils/__init__.py
@@ -5,7 +5,6 @@ units = PhysicsConstants()
 from .construct_body import construct_body
 from .setup_outdir import setup_outdir
 from .sample_powerlaw import sample_powerlaw
-from .is_floatable import is_floatable
 from .make_propagator  import make_propagator
 from .make_initial_thetas import make_initial_thetas
 from .make_initial_e import make_initial_e
