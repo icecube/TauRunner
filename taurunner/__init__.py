79e9eb2089d395b134460caf8a031536aba5111f
jlazar17 <jeff.p.lazar@gmail.com>
2024-04-16 17:53:11 -0500

    import rnu_MC into main branch

--- taurunner/__init__.py
+++ taurunner/__init__.py
@@ -4,4 +4,4 @@ from .utils import __init__
 from .cross_sections import __init__
 from .particle import __init__
 from .proposal_interface import __init__
-from .Casino import Propagate
+from .Casino import Propagate, run_MC
